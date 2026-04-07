07142b32eb64ea30d3750268e2823e2fc5d33429
AKR9634 <akhil6july2003@gmail.com>
2026-04-07 23:11:26 +0530

    LangChain Tools Added!!!

---
+++ LangChain_Tools/Custom_Multiplication_Tool_Using_Tool_decorator.py
@@ -0,0 +1,15 @@
+from langchain_core.tools import tool
+
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers..."""
+    return a*b
+
+result = multiply.invoke({"a":3, "b":5})
+
+print(multiply.name)
+print(multiply.description)
+print(multiply.args)
+print(result)
+
+print(multiply.args_schema.model_json_schema())
